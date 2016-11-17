2ef86eabe085d1e9518fd125b21bb73b5859d5d3
Coleman Rollins <crollins199@gmail.com>
2016-11-17 10:07:06 -0600

    [cr] got excerpts working on block page

--- pages/blog.js
+++ pages/blog.js
@@ -7,25 +7,34 @@ import Helmet from "react-helmet"
 import access from 'safe-access'
 import { config } from 'config'
 import include from 'underscore.string/include'
+import { prune, include as includes } from 'underscore.string'
+import '../css/blogIndex.scss'
 
 class BlogIndex extends React.Component {
   render () {
     const pageLinks = []
+    let body
+    let title
     // Sort pages.
     const sortedPages = sortBy(this.props.route.pages, (page) =>
       access(page, 'data.date')
     ).reverse()
     sortedPages.forEach((page) => {
       if (access(page, 'file.ext') === 'md' && !include(page.path, '/404')) {
-        const title = access(page, 'data.title') || page.path
+        title = access(page, 'data.title') || page.path
+        body = prune(page.data.body.replace(/<[^>]*>/g, ''), 200)
         pageLinks.push(
           <li
+            className='post'
             key={page.path}
             style={{
               marginBottom: rhythm(1/4),
             }}
           >
             <Link style={{boxShadow: 'none'}} to={prefixLink(page.path)}>{title}</Link>
+            <p>
+              {body}
+            </p>
           </li>
         )
       }
@@ -39,7 +48,7 @@ class BlogIndex extends React.Component {
             {"name": "keywords", "content": "blog, articles"},
           ]}
         />
-        <ul>
+        <ul className='blog__posts'>
           {pageLinks}
         </ul>
       </div>
