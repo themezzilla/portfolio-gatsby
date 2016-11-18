35b645f0784d75e9c3a49adc4b0e3e56953b13d7
Coleman Rollins <crollins199@gmail.com>
2016-11-18 09:41:04 -0600

    [cr] import react-icons for footer links

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { prefixLink } from 'gatsby-helpers'
 import { Link } from 'react-router'
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/lib/fa'
 import './footer.scss'
 
 class Footer extends React.Component {
   render () {
     return (
-      <div>
-        Footer
+      <div className='footer'>
+        <FaTwitter />
+        <FaGithub />
+        <FaLinkedin />
       </div>
     )
   }
